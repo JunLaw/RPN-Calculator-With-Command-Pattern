db27a94a4cee2727bea9f63d6355128f622792d4
Dylan <dylandoubi99@gmail.com>
2020-10-16 21:07:37 +0200

    Envoi

---
+++ SimpleFraction/frpack/Main.java
@@ -0,0 +1,12 @@
+package frpack;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Fraction f = new Fraction(2,3);
+        System.out.println(f.toString());
+
+    }
+
+}
